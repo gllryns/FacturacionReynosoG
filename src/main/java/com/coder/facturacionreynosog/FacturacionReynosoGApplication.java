40401f5cdb48ebd1cf841b7ea1301826335dcfad
gllryns <gllryns@gmail.com>
2022-10-09 18:09:41 -0300

    Spring Boot initializer seed

---
+++ src/main/java/com/coder/facturacionreynosog/FacturacionReynosoGApplication.java
@@ -0,0 +1,13 @@
+package com.coder.facturacionreynosog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionReynosoGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacturacionReynosoGApplication.class, args);
+    }
+
+}
